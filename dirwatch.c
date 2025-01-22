a4d6470f797b1e1fe938adeda04410517879eda2
RYANPND <rpaillet@nd.edu>
2025-01-21 22:11:43 -0500

    dirwatch inital

--- dirwatch.c
+++ dirwatch.c
@@ -1,7 +1,36 @@
-#include <stdio.h>
+/**
+ * @file dirwatch.c
+ * @brief list directory contents
+ * @author Ryan Paillet
+ * @date January 20th, 2025
+ */
 
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
 {
-    printf("Hello, world!\n");
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <directory>\n", argv[0]);
+        return 1;
+    }
+
+    DIR *d = opendir(argv[1]);
+    if (!d)
+    {
+        fprintf(stderr, "Unable to open '%s': %s\n", argv[1], strerror(errno));
+        return 1;
+    }
+
+    struct dirent *ent;
+    while ((ent = readdir(d)) != NULL)
+    {
+        printf("%s\n", ent->d_name);
+    }
+    closedir(d);
     return 0;
 }
