021b46a69e391904a8b18e2fd14413ebd71b1870
RYANPND <rpaillet@nd.edu>
2025-01-22 23:49:22 -0500

    dirwatch looping

--- dirwatch.c
+++ dirwatch.c
@@ -1,15 +1,46 @@
 /**
  * @file dirwatch.c
- * @brief list directory contents
- * @author Ryan Paillet
- * @date January 20th, 2025
+ * @brief loops directory
  */
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
+
+static volatile int keep_running = 1;
+
+static void interrupt_handler(int sig)
+{
+    (void)sig;
+    keep_running = 0;
+}
+
+static void clear_screen(void)
+{
+    printf("\033[H\033[J");
+    fflush(stdout);
+}
+
+static void list_once(const char *path)
+{
+    DIR *d = opendir(path);
+    if (!d)
+    {
+        fprintf(stderr, "Unable to open '%s': %s\n", path, strerror(errno));
+        return;
+    }
+    struct dirent *ent;
+    while ((ent = readdir(d)) != NULL)
+    {
+        puts(ent->d_name);
+    }
+    closedir(d);
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,18 +50,21 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    DIR *d = opendir(argv[1]);
-    if (!d)
+    signal(SIGINT, interrupt_handler);
+
+    while (keep_running)
     {
-        fprintf(stderr, "Unable to open '%s': %s\n", argv[1], strerror(errno));
-        return 1;
+        clear_screen();
+        list_once(argv[1]);
+        fflush(stdout);
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (!keep_running)
+                break;
+            sleep(1);
+        }
     }
 
-    struct dirent *ent;
-    while ((ent = readdir(d)) != NULL)
-    {
-        printf("%s\n", ent->d_name);
-    }
-    closedir(d);
     return 0;
 }
