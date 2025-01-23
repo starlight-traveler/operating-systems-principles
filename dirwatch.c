0c516cbfee808dcbbddbd80089b9bcb55e186e39
RYANPND <rpaillet@nd.edu>
2025-01-23 00:11:35 -0500

    adding gui

--- dirwatch.c
+++ dirwatch.c
@@ -1,6 +1,8 @@
 /**
  * @file dirwatch.c
- * @brief loops directory
+ * @brief Adding metadata to the loop
+ * @author Ryan Paillet
+ * @date January 20th, 2025
  */
 
 #define _GNU_SOURCE
@@ -11,6 +13,8 @@
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <pwd.h>
 
 static volatile int keep_running = 1;
 
@@ -26,6 +30,38 @@ static void clear_screen(void)
     fflush(stdout);
 }
 
+static const char *file_type(mode_t mode)
+{
+    if (S_ISREG(mode))
+        return "file";
+    if (S_ISDIR(mode))
+        return "dir";
+    if (S_ISLNK(mode))
+        return "link";
+    if (S_ISBLK(mode))
+        return "blk";
+    if (S_ISCHR(mode))
+        return "chr";
+    if (S_ISFIFO(mode))
+        return "fifo";
+    if (S_ISSOCK(mode))
+        return "sock";
+    return "other";
+}
+
+static void get_owner_name(uid_t uid, char *owner, size_t n)
+{
+    struct passwd *pwd = getpwuid(uid);
+    if (pwd)
+    {
+        snprintf(owner, n, "%s", pwd->pw_name);
+    }
+    else
+    {
+        snprintf(owner, n, "%u", (unsigned)uid);
+    }
+}
+
 static void list_once(const char *path)
 {
     DIR *d = opendir(path);
@@ -34,11 +70,33 @@ static void list_once(const char *path)
         fprintf(stderr, "Unable to open '%s': %s\n", path, strerror(errno));
         return;
     }
+
+    printf("%-20s %10s %-6s %4s %-16s\n", "NAME", "SIZE", "TYPE", "MODE", "OWNER");
+    printf("---------------------------------------------------------------------\n");
+
     struct dirent *ent;
     while ((ent = readdir(d)) != NULL)
     {
-        puts(ent->d_name);
+        char full[1024];
+        snprintf(full, sizeof(full), "%s/%s", path, ent->d_name);
+
+        struct stat st;
+        if (lstat(full, &st) != 0)
+        {
+            fprintf(stderr, "lstat('%s'): %s\n", full, strerror(errno));
+            continue;
+        }
+
+        char owner[64];
+        get_owner_name(st.st_uid, owner, sizeof(owner));
+        printf("%-20s %10lld %-6s %04o %-16s\n",
+               ent->d_name,
+               (long long)st.st_size,
+               file_type(st.st_mode),
+               (unsigned)(st.st_mode & 07777),
+               owner);
     }
+
     closedir(d);
 }
 
